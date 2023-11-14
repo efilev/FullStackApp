0141d52ca735ccb4b26993b93a5f53293b75cbdc
Emma <emmafilv@gmail.com>
2023-11-14 17:07:36 -0500

    Module 4

---
+++ .seedgooserc.js
@@ -0,0 +1,7 @@
+//seedgoserc.js
+module.exports = {
+    modelBaseDirectory:'app_server/models',
+    models: ['*.js', '!db.js'],
+    data: 'data',
+    db: 'mongodb://localhost:27017/traveler'
+};
